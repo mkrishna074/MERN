1f2d14f9adfe69c3490c43ddcd9bbdc609939707
mkrishna074 <muralikrishna.korivi@gmail.com>
2020-06-06 13:06:09 -0400

    adding new store

--- todayi/src/models/item.model.js
+++ todayi/src/models/item.model.js
@@ -1,10 +1,18 @@
 const mongoose = require('mongoose');
+
 const itemSchema = new mongoose.Schema({
+    category: {
+        type: String,
+        required: true,
+        max: 25,
+        unique: true,
+        min: 6
+    },
     title: {
         type: String,
         required: true,
         max: 255,
-        unique: true,
+        trim: true,
         min: 25
     },
     highlights: [{
@@ -12,7 +20,14 @@ const itemSchema = new mongoose.Schema({
     }],
     tags: [{
         type: String
+    }],
+    media: [{
+        type: String
     }]
+},
+{
+  timestamps: true
 })
+
 
 module.exports = mongoose.model('Item', itemSchema);
---
+++ todayi/src/store/actions/eventActions.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+import Item from '../../models/item.model'
+
+import { returnErrors } from './errorActions';
+import {
+  ITEMS_LOADING,
+  ADD_ITEM,
+  DELETE_ITEM,
+  GET_ITEMS,
+  ADDED_ITEM,
+  ITEM_ERROR
+} from './types';
+
+export const addEvent = (item) => (dispatch, getState) => {
+
+    const config = {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      };
+    
+      // Request body
+      const body = JSON.stringify(item);
+
+    // User loading
+    dispatch({ type: ADD_ITEM });
+  
+    axios
+      .post('/api/todayi/add',item, config)
+      .then(res =>
+        dispatch({
+          type: ADDED_ITEM,
+          payload: res.data
+        })
+      )
+      .catch(err => {
+        dispatch(returnErrors(err.response.data, err.response.status));
+        dispatch({
+          type: ITEM_ERROR
+        });
+      });
+  };
