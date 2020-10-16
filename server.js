b11e3b42d202f42012b7ccb534119f2d4aa530af
mkrishna074 <muralikrishna.korivi@gmail.com>
2020-10-15 20:38:04 -0400

    update to port setting

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const commonRouter = require('./routes/common.route');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
